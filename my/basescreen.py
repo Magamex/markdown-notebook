1853efea93ce7b6c60adfbe7b67bdded16f73224
phpusr <phpusr@gmail.com>
2019-10-24 19:14:21 +0500

    add: Note tree viewer

--- my/basescreen.py
+++ my/basescreen.py
@@ -1,5 +1,8 @@
 from kivy.lang import Builder
+from kivy.properties import ObjectProperty
 from kivy.uix.screenmanager import Screen
+from kivy.uix.treeview import TreeViewLabel
+from markdown_tree_parser.parser import parse_file
 
 from my.file_manager import FileManager
 
@@ -8,22 +11,47 @@ class BaseScreen(Screen):
 
     Builder.load_file("my/basescreen.kv")
 
+    note_tree = ObjectProperty()
+
     def init(self, app):
         self.app = app
         self.main_screen = app.screen
         self.create_file_manager()
+        self.note_tree.bind(minimum_height=self.note_tree.setter('height'))
+        if True:
+            self.open_note_tree('/home/phpusr/notes/knowledge-base/linux.md')
 
     def create_file_manager(self):
         self.file_manager = FileManager(
             root_path='/home/phpusr/notes',
-            open_note_editor=self.open_note_editor,
+            select_file_callback=self.open_note_tree,
             exit_from_app=self.exit_from_app
         )
         self.ids['fm'].add_widget(self.file_manager)
         self.file_manager.show_root()
 
-    def open_note_editor(self, note_file):
-        with open(note_file) as f:
+    def open_note_tree(self, note_file_path):
+        self._fill_tree_view(note_file_path)
+        self.ids.manager.current = 'note_tree_screen'
+        self.main_screen.ids.action_bar.left_action_items = [
+            ['chevron-left', lambda x: self.back_screen()]
+        ]
+
+    def _fill_tree_view(self, note_file_path):
+        out = parse_file(note_file_path)
+        self._populate_tree_view(self.note_tree, out)
+
+    def _populate_tree_view(self, tree_view, node, parent=None):
+        if parent is None:
+            tree_node = tree_view.add_node(TreeViewLabel(text=node.title, is_open=True))
+        else:
+            tree_node = tree_view.add_node(TreeViewLabel(text=node.text, is_open=False), parent)
+
+        for child_node in node:
+            self._populate_tree_view(tree_view, child_node, tree_node)
+
+    def open_note_editor(self, note_file_path):
+        with open(note_file_path) as f:
             self.ids.note_editor.text = f.read()
             self.ids.manager.current = 'note_editor_screen'
             self.main_screen.ids.action_bar.left_action_items = [
--- my/file_manager.py
+++ my/file_manager.py
@@ -5,24 +5,17 @@ from kivymd.uix.filemanager import MDFileManager
 
 class FileManager(MDFileManager):
 
-    def __init__(self, root_path, open_note_editor, exit_from_app):
-        super().__init__(
-            exit_manager=self.exit_manager_call,
-            select_path=self.select_path_call,
-            previous=False
-        )
+    def __init__(self, root_path, select_file_callback, exit_from_app):
+        super().__init__(previous=False)
         self.root_path = root_path
-        self.open_note_editor = open_note_editor
+        self.select_file_callback = select_file_callback
         self.exit_from_app = exit_from_app
 
     def show_root(self):
         self.show(self.root_path)
 
-    def exit_manager_call(self, *args):
-        self.exit_from_app()
-
-    def select_path_call(self, path):
-        self.open_note_editor(path)
+    def select_path(self, path):
+        self.select_file_callback(path)
 
     def select_dir_or_file(self, path):
         if os.path.isfile(path):
@@ -35,5 +28,8 @@ class FileManager(MDFileManager):
     def back(self):
         if self.current_path != self.root_path:
             super().back()
+
+    def exit_manager(self, _):
+        self.exit_from_app()
 
 
