ae93a0f01f0d0a669d4a8a85f82a4f6f855b350c
phpusr <phpusr@gmail.com>
2019-10-25 17:03:31 +0500

    add: Confirm and save note

--- my/basescreen.py
+++ my/basescreen.py
@@ -1,6 +1,8 @@
+import os
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
 from kivy.uix.screenmanager import Screen
+from kivymd.uix.dialog import MDDialog
 from markdown_tree_parser.parser import parse_file
 
 from my.widget import FileManager, NoteTreeViewLabel
@@ -22,7 +24,8 @@ class BaseScreen(Screen):
         self.note_editor.bind(minimum_height=self.note_editor.setter('height'))
         if True:
             self._open_note_tree('/home/phpusr/notes/knowledge-base/linux.md')
-            self._open_note_viewer(list(self.note_tree.iterate_all_nodes())[4].note.full_source)
+            self._select_note_heading(list(self.note_tree.iterate_all_nodes())[4])
+            self._open_note_editor()
 
     def _create_file_manager(self):
         self.file_manager = FileManager(
@@ -34,11 +37,11 @@ class BaseScreen(Screen):
         self.file_manager.show_root()
 
     def _open_note_tree(self, note_file_path):
+        self._current_note_file_path = note_file_path
+        self._current_note_file_name = os.path.dirname(note_file_path)
         self._fill_tree_view(note_file_path)
         self.ids.manager.current = 'note_tree_screen'
-        self.main_screen.ids.action_bar.left_action_items = [
-            ['chevron-left', lambda x: self._back_screen()]
-        ]
+        self._set_back_button()
 
     def _fill_tree_view(self, note_file_path):
         out = parse_file(note_file_path)
@@ -47,7 +50,7 @@ class BaseScreen(Screen):
 
     def _populate_tree_view(self, node, parent=None):
         if parent is None:
-            tree_node = self.note_tree.add_node(NoteTreeViewLabel(node.root, is_open=True))
+            tree_node = self.note_tree.add_node(NoteTreeViewLabel(node.main, is_open=True))
         else:
             tree_node = self.note_tree.add_node(NoteTreeViewLabel(node, is_open=False), parent)
 
@@ -59,21 +62,25 @@ class BaseScreen(Screen):
             self.note_tree.remove_node(node)
 
     def _select_note_heading(self, node):
+        self._current_note = node.note
         self._open_note_viewer(node.note.full_source)
 
     def _open_note_viewer(self, text):
         self.note_viewer.text = text
         self.ids.manager.current = 'note_viewer_screen'
-        self.main_screen.ids.action_bar.left_action_items = [
-            ['chevron-left', lambda x: self._back_screen()]
-        ]
+        self._set_back_button()
 
     def _open_note_editor(self):
         self.note_editor.text = self.note_viewer.text
         self.note_editor.cursor = (0, 0)
         self.ids.manager.current = 'note_editor_screen'
+        self._set_back_button(action=self._confirm_save_note)
+
+    def _set_back_button(self, action=None):
+        if action is None:
+            action = self._back_screen
         self.main_screen.ids.action_bar.left_action_items = [
-            ['chevron-left', lambda x: self._back_screen()]
+            ['chevron-left', lambda x: action()]
         ]
 
     def _back_screen(self):
@@ -89,6 +96,23 @@ class BaseScreen(Screen):
             ]
         else:
             raise Exception('Not support screen')
+
+    def _confirm_save_note(self):
+        if self.note_editor.text != self._current_note.full_source:
+            MDDialog(
+                title='Confirm save', size_hint=(.8, .3), text_button_ok='Yes',
+                text=f'Do you want to save {self._current_note_file_name}',
+                text_button_cancel='No',
+                events_callback=self._confirm_save_note_callback
+            ).open()
+
+    def _confirm_save_note_callback(self, answer, _):
+        if answer == 'Yes':
+            with open(self._current_note_file_path, 'w') as f:
+                f.write(self._current_note.root.full_source)
+                self._back_screen()
+        elif answer == 'No':
+            self._back_screen()
 
     def _exit_from_app(self):
         self.app.dialog_exit()
